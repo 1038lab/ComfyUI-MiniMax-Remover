b03cd1a455700b82ee9a289b1604c09295338859
AI Lab <129358391+1038lab@users.noreply.github.com>
2025-06-27 06:15:09 -0700

    Add files via upload

---
+++ Minimax/__init__.py
@@ -0,0 +1,4 @@
+from .pipeline_minimax_remover import Minimax_Remover_Pipeline
+from .transformer_minimax_remover import Transformer3DModel
+
+__all__ = ["Minimax_Remover_Pipeline", "Transformer3DModel"]
